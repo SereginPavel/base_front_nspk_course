bb6f4b2b912ee841b836e9795698f07d11def2df
Pavel Seryogin <Sereginpavel57@yandex.ru>
2022-10-24 23:58:44 +0300

    task4

---
+++ js/edit-form.js
@@ -0,0 +1,8 @@
+const contact = { firstName: 'John', 
+            lastName: 'Rambo', 
+            phoneNumber: '+7(999)999-99-99', 
+            email: 'johnrambo@nspk.ru', 
+            companyTitle: 'NSPK', 
+            jobTitle: 'Developer'}
+
+console.log(contact.firstName + ' ' + contact.lastName + ', ' + contact.jobTitle)
---
+++ js/table.js
@@ -0,0 +1,11 @@
+const contactJohn = { firstName: 'John', 
+            lastName: 'Rambo', 
+            phoneNumber: '+7(999)999-99-99'}
+
+const contactAlan = { firstName: 'Alan', 
+            lastName: 'Jellybean', 
+            phoneNumber: '89054567854'}
+
+const contactJonathan = { firstName: 'Jonathan', 
+            lastName: 'Lollipop', 
+            phoneNumber: '89764563467'}
