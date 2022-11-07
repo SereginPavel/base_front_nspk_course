faa3be2289f4273e6a3f245ce3bdeab6859f5729
Pavel Seryogin <Sereginpavel57@yandex.ru>
2022-11-08 00:34:13 +0300

    удалил лишнее

--- js/modal.js
+++ js/modal.js
@@ -131,8 +131,6 @@ const Modal = {
         this.removeEventListeners();
         document.body.removeChild(this.overlay);
         document.body.removeChild(this.modalContainer);
-        this.modalContainer.classList.add('container_modal_hidden');
-        this.overlay.classList.add('modal-overlay_hidden');
 
         setTimeout(() => {
             
