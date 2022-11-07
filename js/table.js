0ef7ec1f58fef56a339e08a1b48254eab1a2662b
Pavel Seryogin <Sereginpavel57@yandex.ru>
2022-11-08 02:33:56 +0300

    task 6

--- js/table.js
+++ js/table.js
@@ -16,6 +16,53 @@ const contactJonathan = {
     phoneNumber: '89764563467'
 }
 
+const contacts = [contactJohn, contactAlan, contactJonathan];
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.contacts_count').innerText = getContactsCount();
+    document.querySelector('.tabs a').innerText = getAll();
+
+    const tableBody = document.querySelector('table tbody');
+    contacts.forEach( contact => {
+        const row = createTableRow(tableBody);
+        Object.values(contact).forEach( value => createTableCellWithValue(row, value));
+        createTableCellWithPancil(row);
+    })
+
+});
+
+function createTableRow(table){
+    const row = document.createElement('tr');
+    table.append(row);
+    return row;
+}
+
+function createTableCellWithValue(row, value){
+    const cell = document.createElement('td');
+    cell.innerText = value;
+    row.append(cell);
+}
+
+function createTableCellWithPancil(row){
+    const cell = document.createElement('td');
+    cell.classList.add('contacts_button_edit');
+    cell.innerHTML = "<a href='../html/edit-form.html' style='background-color: none; cursor: pointer;'><img src='../img/edit.svg' alt=''>";
+    row.append(cell);
+}
+
+function getContactsCount(){
+    if (contacts.length == 1 ) {
+        return contacts.length + ' Contact';
+    } else {
+        return contacts.length + ' Contacts';
+    }
+}
+
+function getAll(){
+    return 'All ' + contacts.length;
+}
+
 function showModal(){
     Modal.init({
         header: 'Create new contact',
